0810d83e4ac10519bf0fa829737fd7f7261966a4
Horace Chan <horace.chan@pmcs.com>
2017-01-11 14:46:34 -0800

    update

--- uvm_examples/integrated/codec/debug.cmd
+++ uvm_examples/integrated/codec/debug.cmd
@@ -1,16 +1,24 @@
 # house keeping functions
 help
 run 10ns
+run 20ns
 pause
 call debug_prompt
 history
 repeat 1
 history clear
 history
-history clear
 
 # register access
 rd_addr 0
+rd_addr 4 
+
+# load command file
+history save demo1
+read demo1
+
+wr_addr 4 1
+rd_addr 4 
 rd_reg regmodel.IntMask 
 wr_reg regmodel.IntMask 0
 rd_reg regmodel.IntMask 
@@ -18,9 +26,7 @@ rd_regfld regmodel.TxStatus.TxEn
 wr_regfld regmodel.TxStatus.TxEn 0
 rd_regfld regmodel.TxStatus.TxEn
 
-# load command file
-history save demo1
-read demo1
+#rewind
 
 # continue to demo sequence commands
 continue
@@ -28,7 +34,6 @@ run 20000ns
 
 # send a sequence item
 seq_item_create vip_tr tr
-seq_item_rand tr
 seq_item_set_fields tr chr='hff
 seqr_execute_item env.vip.sqr tr
 
@@ -37,9 +42,6 @@ run 20000ns
 # start a new sequence
 seq_create vip_my01_seq my01_seq
 seq_rand my01_seq
-
-run 20000ns
-
 seq_start -priority 200 -new_thread 1 my01_seq env.vip.sqr
 
 run 20000ns
